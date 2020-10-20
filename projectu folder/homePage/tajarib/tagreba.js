3d1706b600749e33af81106703169bc95ab582e6
waashii <70151834+waashii@users.noreply.github.com>
2020-10-20 18:27:13 +0300

    added project folder

---
+++ projectu folder/homePage/tajarib/tagreba.js
@@ -0,0 +1,244 @@
+
+// function myFunction() {
+//     let text = "HELLO WORLD";
+    
+//     let i = 0;
+//     for(; i < text.length; i++) {
+//       let washi = text.charAt(i);
+//       document.getElementById("pi").innerHTML += washi ;
+//       let char = text.charAt(i);
+
+//       console.log(char)
+//       setTimeout(() => {
+          
+//         if(char == "A") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/A.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+      
+    
+      
+//       setTimeout(() => {
+          
+//      if (char == "B") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/B.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "C") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/C.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "D") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/D.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "E") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/E.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "F") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/F.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "G") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/G.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "H") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/H.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "I") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/I.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "J") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/J.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "K") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/K.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "L") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/L.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "M") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/M.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "N") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/N.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "O") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/O.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "P") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/P.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "Q") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/Q.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "R") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/R.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "S") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/S.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "T") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/T.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "U") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/U.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "V") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/V.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "W") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/W.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "X") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/X.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "Y") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/Y.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+//     setTimeout(() => {
+          
+//         if(char == "Z") {
+//           let bleep = new Audio();
+//           bleep.src = "../basicsPages/voices/Z.mp3";
+//           bleep.play()
+//         }
+//     }, 2000);
+
+// }
+    
+// }
+const quiz = [
+  {
+    q:'تتوقع شنو مساري؟',
+    options:['مسار ال WEB✨','ios','android','مالي شغل🤓'],
+    answer:0
+  },
+  {
+    q:'هل تعرف من هو الشخص الذي اخترع لغة مورس كود؟',
+    options:['قيتار','هتلر','رادار','🤓اتوقع شسمه هذا كلتر'],
+    answer:1
+  },
+  {
+    q:' ورنا شطارتك و فج (ترجم) الشفرة هذي الى اللغة الانجليزية',
+    options:['what','تعبت','i dont know',' شسمه  '],
+    answer:'i hope you have fun learning did you know that their is a video for you to watch'
+  },
+
+]
